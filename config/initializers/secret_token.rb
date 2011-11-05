06a43e80b4eff8c5e9390877c2cc32f814c27068
Jean-Paul Bernadina <jpbernadina@gmail.com>
2011-11-05 13:39:06 -0430

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lekkerlekker::Application.config.secret_token = '933e045f6b0574e3f2d8c26835319b1baf4ef5e6412c6a2b65479fcf7372faaa4162875c31be9aec3d8de87d499037b000857fb5937867f0c9c7821b3d0d16e7'
